702e6fb16099496e36d3233dea8be2051f8ffbbe
CarlosCrispin <carlos.crispin.cc@gmail.com>
2021-06-17 20:21:08 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DetallesComponent } from './pages/item/detalles/detalles.component';
+import { EditarComponent } from './pages/item/editar/editar.component';
+import { ListaComponent } from './pages/item/lista/lista.component';
+import { NuevoComponent } from './pages/item/nuevo/nuevo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'lista', component: ListaComponent},
+  {path: 'nuevo', component: NuevoComponent},
+  {path: 'editar', component: EditarComponent},
+  {path: 'detalles', component: DetallesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListaComponent } from './pages/item/lista/lista.component';
+import { NuevoComponent } from './pages/item/nuevo/nuevo.component';
+import { EditarComponent } from './pages/item/editar/editar.component';
+import { DetallesComponent } from './pages/item/detalles/detalles.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListaComponent,
+    NuevoComponent,
+    EditarComponent,
+    DetallesComponent
   ],
   imports: [
     BrowserModule,
